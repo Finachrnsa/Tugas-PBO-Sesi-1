45b63034aeafa6090b9060570704db11555cb86c
Finachrnsa <146912038+Finachrnsa@users.noreply.github.com>
2025-05-05 20:10:02 +0700

    Add files via upload

---
+++ BelajarDialog.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class BelajarDialog {
+    public static void main(String[] args) {
+
+        String pelajaran = JOptionPane.showInputDialog(null, "Anda sedang belajar apa?", "Input", JOptionPane.QUESTION_MESSAGE);
+
+
+        JOptionPane.showMessageDialog(null, "Belajar " + pelajaran + " sangat mudah", "Message", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
